35cd013f8f4167a0650427df9d158286d340cdd3
wojtek0126 <wojtek0126@gmail.com>
2020-09-25 18:20:14 +0200

    zadanie 2 gotowe

---
+++ 2-menu/js/app.js
@@ -0,0 +1,23 @@
+const $li = document.getElementsByClassName('nav-el');
+
+function toggleOnClick(element_to_click,  element_to_toggle, list_of_elements, class_name) {
+    element_to_click.addEventListener('click', function (e) {
+        e.preventDefault();
+        for(i = 0;i < list_of_elements.length;i++){
+            if (list_of_elements[i].classList.contains(class_name)){
+                list_of_elements[i].classList.remove(class_name);
+            }
+        }
+        element_to_toggle.classList.toggle(class_name);
+    })
+}
+
+for (i = 0;i <$li.length;i++) {
+    toggleOnClick($li[i], $li[i], $li,'nav-el-active');
+}
+
+
+
+
+
+
