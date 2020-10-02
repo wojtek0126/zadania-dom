22b3b5c053d3700855c58a177a701074e919a46f
wojtek0126 <wojtek0126@gmail.com>
2020-10-02 15:41:41 +0200

    zadanie 6 elementy z clone delete i numerami po kolei, brakuje efektu dla przyciskow delete i clone

--- 6-zadanie-z-lista/js/app.js
+++ 6-zadanie-z-lista/js/app.js
@@ -1,28 +1,17 @@
-// Po kliknięciu w "dodaj element" do listy dodaj nowy element podobny do tego już istniejącego na liście
-//
-// Po dodaniu powinien mieć kolejny numer który wylicz na podstawie ilości elementów + 1
-//
-// Po kliknięciu w ikonę kosza usuń dany element
-// Po kliknięciu w ikonę klonowania sklonuj na koniec listy dany element
 
-function print(element) {
-    console.log(element);
-}
-
-function getElement(attrValue) {
+const getElement = (attrValue) => {
     return document.querySelector(attrValue);
 }
 
-function getElementById(attrValue) {
+const getElementById = (attrValue) => {
     return document.getElementById(attrValue);
-
 }
 
-function getAAll(attrValue) {
+const getAAll = (attrValue) => {
     return document.querySelectorAll(attrValue);
 }
 
-function newElement(tag, attributes) {
+const newElement = (tag, attributes) => {
     const element = document.createElement(tag);
     for (attr in attributes) {
         element.setAttribute(attr, attributes[attr]);
@@ -31,59 +20,63 @@ function newElement(tag, attributes) {
 }
 
 
+
 const addBtn = getElementById('add');
-print(addBtn);
 const list = getElement('.list');
-print(list);
 const element = getElement('.element');
-print(element);
-const elementTitle = getElement('.element-title');
-print(elementTitle);
 const elementNumber = getElement('.nr');
-print(elementNumber);
-const trash = getElement('.delete');
-print(trash);
-const clone = getElement('.clone');
-print(clone);
-const elements = getAAll('.element');
-print(elements);
+const cloneBtn = getElement('.clone');
 
 let i = parseFloat(elementNumber.innerText);
-print(i);
 
-
-addBtn.addEventListener('click', function () {
-    const newEl = newElement('div', {class: 'element'});
-    const newElText = newElement('h3', {class: 'element-title'});
-    const newElNumber = newElement('span', {class: 'nr'});
-    const newElCloneBtn = newElement('button', {class: 'clone'});
-    const newElTrashBtn = newElement('button', {class: 'delete'});
-    newElText.innerText = 'Element numer';
-    newElNumber.innerText =` ${i+1}`;
-    list.appendChild(newEl);
-    newEl.appendChild(newElText);
-    newElText.appendChild(newElNumber);
-    newEl.appendChild(newElCloneBtn);
-    newEl.appendChild(newElTrashBtn);
-    print('clicked');
-    i++;
-})
+const addElementEnabled = () => {
+    addBtn.addEventListener('click', function () {
+        const btn = addBtn;
+        btn.style.background = 'gold';
+        setTimeout(function () {
+            btn.style.background = '#3689DB';
+        }, 100);
+        const newEl = newElement('div', {class: 'element'});
+        const newElText = newElement('h3', {class: 'element-title'});
+        const newElNumber = newElement('span', {class: 'nr'});
+        const newElCloneBtn = newElement('button', {class: 'clone'});
+        const newElTrashBtn = newElement('button', {class: 'delete'});
+        newElText.innerText = 'Element numer';
+        newElNumber.innerText =` ${i+1}`;
+        list.appendChild(newEl);
+        newEl.appendChild(newElText);
+        newElText.appendChild(newElNumber);
+        newEl.appendChild(newElCloneBtn);
+        newEl.appendChild(newElTrashBtn);
+        i++;
+    })
+}
 
 const deleteElemEnabled = (userList, classOfDeleteButton) => {
     userList.addEventListener('click', function (e) {
         const elements = getAAll('.element');
-        print(elements);
         const target = e.target;
         if (target.className == classOfDeleteButton ) {
             const li = target.parentElement;
-            userList.removeChild(li);
-        }
+            userList.removeChild(li);        }
         if(elements.length < 2) {
             i = 0;
         }
     })
 }
 
+const cloneElementEnabled = (userList, classOfCloneButton) => {
+    userList.addEventListener('click', function (e) {
+        const target = e.target;
+        if (target.className == classOfCloneButton ) {
+            const clone = target.parentElement.cloneNode(true);
+            userList.appendChild(clone);
+        }
+    })
+}
+
+addElementEnabled();
 deleteElemEnabled(list, 'delete');
+cloneElementEnabled(list, 'clone');
 
 
